4bb4abd26c56d11b1eeb3da5d044371e56ef7393
MichellAvilesL <jupiter99mus@gmail.com>
2022-08-03 10:01:57 -0500

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
